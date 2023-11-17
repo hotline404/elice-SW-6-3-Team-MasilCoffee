2baa601ae81e80a70f79efbf94e6ce28c438deab
Kim yeongjun <cosmoyj7733@gmail.com>
2023-11-17 20:38:27 +0900

    fix: amend confirmpasswordForm

--- client/src/pages/mypage/confirmPassword/ConfirmPasswordForm.jsx
+++ client/src/pages/mypage/confirmPassword/ConfirmPasswordForm.jsx
@@ -3,33 +3,42 @@ import Input from "../UI/Input";
 import { CenterBox } from "../mypageComponent/MyPage.style";
 import * as S from "./ConfirmPassword.style";
 
+function ConfirmPasswordForm({ emailRef, passwordRef }) {
+  const InputData = [
+    {
+      ref: emailRef,
+      name: "이메일",
+      type: "email",
+      id: "emailIsValid",
+      placeholder: "이메일을 입력하세요",
+    },
+    {
+      ref: passwordRef,
+      name: "비밀번호",
+      type: "password",
+      id: "passwordIsValid",
+      placeholder: "비밀번호를 입력하세요",
+    },
+  ];
 
-function ConfirmPasswordForm({emailRef, passwordRef}) {
   return (
     <div>
       <CenterBox>
-        <S.InputBox>
-          <Input
-            ref={emailRef}
-            input={{
-              name: "이메일",
-              type: "email",
-              id: "emailIsVaild",
-              placeholder: "이메일을 입력하세요",
-            }}
-          />
-        </S.InputBox>
-        <S.InputBox>
-          <Input
-            ref={passwordRef}
-            input={{
-              name: "비밀번호",
-              type: "password",
-              id: "passwordIsVaild",
-              placeholder: "비밀번호를 입력하세요",
-            }}
-          />
-        </S.InputBox>
+        {InputData.map((data) => {
+          return (
+            <S.InputBox>
+              <Input
+                ref={data.ref}
+                input={{
+                  name: data.name,
+                  type: data.type,
+                  id: data.id,
+                  placeholder: data.placeholder,
+                }}
+              />
+            </S.InputBox>
+          );
+        })}
       </CenterBox>
     </div>
   );

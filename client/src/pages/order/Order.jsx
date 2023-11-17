ac23773b3fe56fd3577941639df7aca92fa07716
김 다빈 <besesa95@gmail.com>
2023-11-17 16:48:39 +0900

    Add : Order update

--- client/src/pages/order/Order.jsx
+++ client/src/pages/order/Order.jsx
@@ -1,13 +1,56 @@
+import React, { useState } from "react";
 import Button from "../../components/ui/button/Button";
 import Card from "../../components/ui/card/Card";
-import { StyledOrder, StyledCategory, CategoryButton } from "./Order.style";
+import Payment from "../payment/Payment";
+import { StyledOrder } from "./Order.style";
 import mockupData from "../../components/ui/data/mockupdata.json";
+import Slider from "react-slick";
 
 const Order = () => {
-  // 목업 가져오기
-  const cards = mockupData.products.map((product, id) => (
-    <Card key={id} data={product} />
-  ));
+  // 카테고리 필터링 하기
+  const [selectedCategory, setSelectedCategory] = useState("전체");
+  const [filteredProducts, setFilteredProducts] = useState(mockupData.products);
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+
+    let filtered;
+    switch (category) {
+      case "전체":
+        filtered = mockupData.products;
+        break;
+      case "에스프레소":
+        filtered = mockupData.products.filter((pd) => pd.id >= 1 && pd.id <= 9);
+        break;
+      case "논커피":
+        filtered = mockupData.products.filter(
+          (pd) => pd.id >= 10 && pd.id <= 16
+        );
+        break;
+      case "스무디":
+        filtered = mockupData.products.filter(
+          (pd) => pd.id >= 17 && pd.id <= 22
+        );
+        break;
+      case "티":
+        filtered = mockupData.products.filter(
+          (pd) => pd.id >= 23 && pd.id <= 30
+        );
+        break;
+      case "에이드":
+        filtered = mockupData.products.filter(
+          (pd) => pd.id >= 31 && pd.id <= 34
+        );
+        break;
+      case "즐겨찾기":
+        filtered = [];
+        break;
+
+      default:
+        filtered = mockupData.products;
+    }
+
+    setFilteredProducts(filtered);
+  };
 
   // 카테고리 리스트 배열
   const categories = [
@@ -20,14 +63,65 @@ const Order = () => {
     "즐겨찾기",
   ];
 
+  // 슬라이드 설정
+  const settings = {
+    arrows: false,
+    swipeToSlide: true,
+    draggable: true,
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <StyledOrder>
-      <StyledCategory>
-        {categories.map((category, index) => (
-          <CategoryButton key={index}>{category}</CategoryButton>
+      <div className="button">
+        <Slider {...settings}>
+          {categories.map((category, index) => (
+            <button
+              key={category}
+              className={`${index === 0 ? "first-button " : ""}${
+                index === categories.length - 1 ? "last-button " : ""
+              }${category === selectedCategory ? "selected-button" : ""}`}
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category}
+            </button>
+          ))}
+        </Slider>
+      </div>
+      <div className="cards-container">
+        {filteredProducts.map((product) => (
+          <Card key={product.id} data={product} />
         ))}
-      </StyledCategory>
-      <div className="cards-container">{cards}</div>
+      </div>
       <div className="buttons-container">
         <Button
           type="grey"
@@ -36,13 +130,7 @@ const Order = () => {
             alert("장바구니로 이동하시겠습니까?");
           }}
         />
-        <Button
-          type="red"
-          text={"결제하기"}
-          onClick={() => {
-            alert("주문하시겠습니까?");
-          }}
-        />
+        <Button type="red" text={"결제하기"} onClick={<Payment />} />
       </div>
     </StyledOrder>
   );

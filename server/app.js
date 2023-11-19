7b93b807a8b8e26fa7b06efe97ee00cbf8127d33
JEONGSUJONG <jeon95ujong@gmail.com>
2023-11-19 18:29:46 +0900

    Path, Cors

--- server/app.js
+++ server/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 const PingRouter = require("./routes/PingRouter");
 const ProductRouter = require("./routes/ProductRoute");

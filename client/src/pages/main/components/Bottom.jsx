593f66607096a3d5f2c0aafe8d9139723bf238d0
김 다빈 <besesa95@gmail.com>
2023-11-20 18:00:06 +0900

    Add : MainPage update

---
+++ client/src/pages/main/components/Bottom.jsx
@@ -0,0 +1,9 @@
+const Bottom = () => {
+  return (
+    <div>
+      <h1>Bottom입니다.</h1>
+    </div>
+  );
+};
+
+export default Bottom;
---
+++ client/src/pages/main/components/MiddleOne.jsx
@@ -0,0 +1,9 @@
+const MiddleOne = () => {
+  return (
+    <div>
+      <h1>MiddleOne입니다.</h1>
+    </div>
+  );
+};
+
+export default MiddleOne;

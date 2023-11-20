96a6b410c1ca9e1a83f9d97f04e0587ff330dce9
김 다빈 <besesa95@gmail.com>
2023-11-20 16:47:56 +0900

    Add : SquareButton style update

---
+++ client/src/components/ui/button/SquareButton.jsx
@@ -0,0 +1,19 @@
+import { StyledButton } from "./SquareButton.style";
+
+const Button = ({ text, type, onClick }) => {
+  const btnType = ["grey", "red"].includes(type) ? type : "grey";
+  return (
+    <StyledButton
+      className={["Button", `Button_${btnType}`].join(" ")}
+      onClick={onClick}
+    >
+      {text}
+    </StyledButton>
+  );
+};
+
+Button.defaultProps = {
+  type: "grey",
+};
+
+export default Button;
---
+++ client/src/components/ui/button/SquareButton.style.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  cursor: pointer;
+  border: none;
+  border-radius: 10px;
+  padding: 12px 35px;
+  font-size: 15px;
+  margin: 0 15px;
+
+  &.Button_grey {
+    background-color: #ececec;
+    color: #650818;
+    font-weight: bold;
+  }
+  &.Button_red {
+    background-color: #650818;
+    color: white;
+    font-weight: bold;
+  }
+`;

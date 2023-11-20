c61e0ca600fa654ddbee1b928d15fd14caa4ee68
Kim yeongjun <cosmoyj7733@gmail.com>
2023-11-20 12:16:34 +0900

    feat: banner

---
+++ client/src/components/layout/banner/Banner.style.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledBanner = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+`

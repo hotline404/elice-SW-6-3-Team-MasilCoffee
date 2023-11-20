c051673dd99caba9c2a49a03537dc3a9f22106fb
YuJin <dldbwls3566@gmail.com>
2023-11-20 21:23:40 +0900

    admin headermargin 수정

--- client/src/pages/admin/adminMenu/AdminMenu.style..jsx
+++ client/src/pages/admin/adminMenu/AdminMenu.style..jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   justify-content: space-between;
-  margin-top: 88px;
   background-color: #f9f9f9;
 `;
 

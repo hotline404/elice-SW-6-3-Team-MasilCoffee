1680b30ffaee780b598ad8ffbb4ef2239605fc0f
김 다빈 <besesa95@gmail.com>
2023-11-21 01:55:18 +0900

    Add : main page bottom update

--- client/src/pages/main/components/style/Bottom.style.jsx
+++ client/src/pages/main/components/style/Bottom.style.jsx
@@ -1,1 +1,82 @@
 import styled from "styled-components";
+
+export const Bottom = styled.div``;
+export const BottomBox = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: normal;
+  > div > b {
+    font-size: 20px;
+  }
+  > div > p {
+    font-size: 17px;
+  }
+  @media (max-width: 768px) {
+    padding: 5px;
+    font-size: smaller;
+  }
+`;
+export const BottomSlide = styled.div`
+  .slick-slider {
+    overflow-x: hidden;
+    @media (max-width: 768px) {
+      flex-direction: column;
+    }
+  }
+
+  // 네비게이션 버튼 숨김
+  .slick-prev,
+  .slick-next {
+    display: none;
+  }
+
+  .slick-track {
+    display: flex;
+    justify-content: space-between;
+    width: 480px;
+  }
+
+  .slick-track > div {
+    box-sizing: border-box;
+    height: autopx;
+    border-radius: 10px;
+    background-color: #8e0e28;
+    word-wrap: break-word;
+    overflow-wrap: break-word;
+    color: white;
+    cursor: pointer;
+    margin-right: 10px;
+    margin-left: 10px;
+    display: flex;
+    align-items: center;
+  }
+  .slick-track > div > div {
+    display: block;
+    padding: 20px;
+  }
+  .slick-track > div > div > div {
+    display: block;
+  }
+`;
+
+export const SquareButtonBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+export const SquareButton = styled.div`
+  margin-right: 10px;
+  padding: 10px 0;
+  background-color: ${(props) => props.bgColor};
+  > Button {
+    font-size: 14px;
+  }
+`;
+export const BsChat = styled.div`
+  padding-top: 10px;
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+  > span {
+    padding-left: 10px;
+  }
+`;

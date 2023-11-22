77053d6db254710cfaaf3736306990d4dd7e0a03
김 다빈 <besesa95@gmail.com>
2023-11-23 01:18:01 +0900

    Add : Action reducer update

--- client/src/redux/action/orderOptionAction.jsx
+++ client/src/redux/action/orderOptionAction.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { ORDEROPTION_TYPE } from "./_types";
 
-function orderOptionAction() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default orderOptionAction
+export const actionSetShotOption = (shotOption) => {
+  return {
+    type: ORDEROPTION_TYPE.ADD_SHOT_ORDEROPTION,
+    payload: shotOption,
+  };
+};
+export const actionSetSyrupsOption = (syrupsOption) => {
+  return {
+    type: ORDEROPTION_TYPE.ADD_SYRUPS_ORDEROPTION,
+    payload: syrupsOption,
+  };
+};
+export const actionSetIceOption = (iceOption) => {
+  return {
+    type: ORDEROPTION_TYPE.ADD_ICE_ORDEROPTION,
+    payload: iceOption,
+  };
+};
+export const actionSetWhippingOption = (whippingOption) => {
+  return {
+    type: ORDEROPTION_TYPE.ADD_WHIPPING_ORDEROPTION,
+    payload: whippingOption,
+  };
+};
+export const actionSetDrizzleOption = (drizzleOption) => {
+  return {
+    type: ORDEROPTION_TYPE.ADD_DRIZZLE_ORDEROPTION,
+    payload: drizzleOption,
+  };
+};
+export const actionSetMilkOption = (milkOption) => {
+  return {
+    type: ORDEROPTION_TYPE.ADD_MILK_ORDEROPTION,
+    payload: milkOption,
+  };
+};
--- client/src/redux/reducer/orderOption.jsx
+++ client/src/redux/reducer/orderOption.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import { ORDEROPTION_TYPE } from "../action/_types";
 
-function oderOption() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const initialState = {
+  shot: 0,
+  syrups: {
+    vanilla: 0,
+    hazelnut: 0,
+    caramel: 0,
+  },
+  ice: "없음",
+  whipping: "없음",
+  drizzle: "없음",
+  milk: "없음",
+};
+const oderOption = (state = initialState, action) => {
+  switch (action.type) {
+    case ORDEROPTION_TYPE.ADD_SHOT_ORDEROPTION:
+      return {
+        ...state,
+        shot: action.payload,
+      };
+    case ORDEROPTION_TYPE.ADD_SYRUPS_ORDEROPTION:
+      return {
+        ...state,
+        syrups: action.payload,
+      };
+    case ORDEROPTION_TYPE.ADD_ICE_ORDEROPTION:
+      return {
+        ...state,
+        ice: action.payload,
+      };
 
-export default oderOption
+    case ORDEROPTION_TYPE.ADD_WHIPPING_ORDEROPTION:
+      return {
+        ...state,
+        whipping: action.payload,
+      };
+    case ORDEROPTION_TYPE.ADD_DRIZZLE_ORDEROPTION:
+      return {
+        ...state,
+        drizzle: action.payload,
+      };
+    case ORDEROPTION_TYPE.ADD_MILK_ORDEROPTION:
+      return {
+        ...state,
+        milk: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+export default oderOption;

90b7254752237e59d4d09c8acd64bb8094abced2
김 다빈 <besesa95@gmail.com>
2023-11-23 01:18:42 +0900

    Add : Order type update

--- client/src/redux/action/_types.jsx
+++ client/src/redux/action/_types.jsx
@@ -4,3 +4,13 @@ export const PRODUCT_TYPE = {
   UPDATE_PRODUCTS: "update_products",
   DELETE_PRODUCT: "delete_product",
 };
+
+export const ORDEROPTION_TYPE = {
+  ADD_ORDEROPTION: "add_orderoption",
+  ADD_SHOT_ORDEROPTION: "add_shot_orderoption",
+  ADD_SYRUPS_ORDEROPTION: "add_syrups_orderoption",
+  ADD_ICE_ORDEROPTION: "add_ice_orderoption",
+  ADD_WHIPPING_ORDEROPTION: "add_whipping_orderoption",
+  ADD_DRIZZLE_ORDEROPTION: "add_drizzle_orderoption",
+  ADD_MILK_ORDEROPTION: "add_milk_orderoption",
+};

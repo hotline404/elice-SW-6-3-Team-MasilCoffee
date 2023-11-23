26b39751537b5a418245d6c536296fed26979118
YuJin <dldbwls3566@gmail.com>
2023-11-23 17:36:45 +0900

    Add rootReducer 수정

--- client/src/redux/action/_types.jsx
+++ client/src/redux/action/_types.jsx
@@ -4,3 +4,29 @@ export const PRODUCT_TYPE = {
   UPDATE_PRODUCTS: "update_products",
   DELETE_PRODUCT: "delete_product",
 };
+
+export const OPTION_TYPE = {
+  GET_ALL_OPTIONS: "get_all_options",
+  GET_OPTION: "get_option",
+  ADD_OPTION: "add_option",
+  UPDATE_OPTION: "update_option",
+  DELETE_OPTION: "delete_option",
+};
+
+export const BOARD_TYPE = {
+  GET_ALL_BOARDS: "get_all_boards",
+  GET_CATEGORY_BOARDS: "get_category_boards",
+  GET_FILTER_BOARDS: "get_filter_boards",
+  GET_MY_BOARDS: "get_my_boards",
+  ADD_BOARD: "add_board",
+  UPDATE_BOARD: "update_board",
+  DELETE_BOARD: "delete_board",
+};
+
+export const COMMENT_TYPE = {
+  GET_BOARD_COMMENTS: "get_board_comments",
+  GET_MY_COMMENTS: "get_my_comments",
+  ADD_COMMENT: "add_comment",
+  UPDATE_COMMENT: "update_comment",
+  DELETE_COMMENT: "delete_comment",
+};

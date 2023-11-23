487800a711e73846c61bab02c2c1caebe44b07e6
김 다빈 <besesa95@gmail.com>
2023-11-24 01:33:25 +0900

    Add : modal 과 ModalContents 컴포넌트 분리

--- client/src/pages/order/components/modal/Modal.jsx
+++ client/src/pages/order/components/modal/Modal.jsx
@@ -1,79 +1,18 @@
-import React, { useState } from "react";
-
-import Button from "../../../../components/ui/button/Button";
-import { Link } from "react-router-dom";
-
 import {
   StyledModal,
   StyleModalContainer,
   StyleX,
-  StyleText,
-  StyleImg,
-  StyleInfo,
-  StyleDisplay,
-  StylePaddingSpan,
-  StyleQuantity,
-  StyleButton,
   StyleXContainer,
 } from "./Modal.style";
 
-import ShotOptionSlide from "./components/ShotOptionSlide";
-import SyrupOptionSlide from "./components/SyrupOptionSlide";
-import IceOptionSlide from "./components/IceOptionSlide";
-import WhippingOptionSlide from "./components/Whipping";
-import DrizzleOptionSlide from "./components/Drizzle";
-
-import MilkOptionSlide from "./components/MilkOptionSlide";
-
-const Modal = ({ openModal, closeModal, data }) => {
-  const [increase, setIncrease] = useState(1);
-  const handleIncreaseOnClick = () => {
-    setIncrease(increase + 1);
-  };
-  const handleDecreaseOnClick = () => {
-    if (increase > 1) {
-      setIncrease(increase - 1);
-    }
-  };
-  const totalPrice = data.price * increase;
-
+const Modal = ({ closeModal, children }) => {
   return (
     <StyledModal>
       <StyleModalContainer>
         <StyleXContainer>
           <StyleX onClick={closeModal}>X</StyleX>
         </StyleXContainer>
-        <StyleText>
-          <StyleImg></StyleImg>
-          <StyleInfo>
-            <span>{data.name}</span>
-            <StylePaddingSpan>{totalPrice}</StylePaddingSpan>
-            <StyleQuantity>
-              <button onClick={handleIncreaseOnClick}>+</button>
-              <span>{increase}</span>
-              <button onClick={handleDecreaseOnClick}>-</button>
-            </StyleQuantity>
-            <StyleDisplay>
-              <span>{data.temp}</span>
-              <span>{data.size}</span>
-            </StyleDisplay>
-          </StyleInfo>
-        </StyleText>
-        <ShotOptionSlide />
-        <SyrupOptionSlide />
-        <IceOptionSlide />
-        <WhippingOptionSlide />
-        <DrizzleOptionSlide />
-        <MilkOptionSlide />
-        <b>총가격 : </b>
-        <StyleButton>
-          <Link to="/Cart">
-            <Button type="grey" text={"장바구니"} />
-          </Link>
-          <Link to="/Payment">
-            <Button type="red" text={"결제하기"} />
-          </Link>
-        </StyleButton>
+        {children}
       </StyleModalContainer>
     </StyledModal>
   );

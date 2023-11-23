39d8f6107f9b6f86ec37d31b4b12fb592d1f2a6e
김 다빈 <besesa95@gmail.com>
2023-11-24 01:41:00 +0900

    Add : _types update

--- client/src/redux/action/_types.jsx
+++ client/src/redux/action/_types.jsx
@@ -6,11 +6,22 @@ export const PRODUCT_TYPE = {
 };
 
 export const ORDEROPTION_TYPE = {
-  ADD_ORDEROPTION: "add_orderoption",
+  ADD_MENU_ORDEROPTION: "add_menu_orderoption",
   ADD_SHOT_ORDEROPTION: "add_shot_orderoption",
   ADD_SYRUPS_ORDEROPTION: "add_syrups_orderoption",
   ADD_ICE_ORDEROPTION: "add_ice_orderoption",
   ADD_WHIPPING_ORDEROPTION: "add_whipping_orderoption",
   ADD_DRIZZLE_ORDEROPTION: "add_drizzle_orderoption",
   ADD_MILK_ORDEROPTION: "add_milk_orderoption",
+  RESET_OPTION: "reset_option",
+};
+
+export const ORDERS_TYPE = {
+  ADD_ORDER: "add_order",
+  REMOVE_ORDER: "remove_order",
+  UPDATE_ORDER: "update_order",
+};
+export const PAYMENT_TYPE = {
+  PAYMENT: "payment",
+  RESET: "reset"
 };

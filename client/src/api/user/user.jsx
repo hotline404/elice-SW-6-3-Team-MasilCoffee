16f41a437959c0e74f5a5f00f6f0c292cb227977
Kim yeongjun <cosmoyj7733@gmail.com>
2023-11-24 14:37:16 +0900

    feat: register

--- client/src/api/user/user.jsx
+++ client/src/api/user/user.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const urladminId = "http://localhost:5000/api/v1/users/admin/:userId"
-const urluser = "http://localhost:5000/api/v1/users";
+const urluser = "http://localhost:5000/api/v1/users"; 
 
 
 export const axiosGetUser = async (token) => {
@@ -18,6 +18,35 @@ export const axiosGetUser = async (token) => {
   return user
 }
 
+
+export const axiosPatchUser = async (token, newNickname, newPhone) => {
+
+  const res = await axios.patch(urluser, {
+    headers: {
+      "Authorization": `Bearer ${token}`,
+      "nickname": newNickname,
+      "phone": newPhone
+    }
+  })
+
+  const newUser = res.data.data;
+
+  return newUser
+}
+
+export const axiosDelUser = async (token) => {
+
+  const res = await axios.del(urluser, {
+    headers: {
+      "Authorization": `Bearer ${token}`,
+    }
+  })
+
+  const delUser = res.data.data;
+
+  return delUser
+}
+
 export const axiosGetAdmin = async (token) => {
 
   const res = await axios.get(urladminId, {
@@ -29,5 +58,36 @@ export const axiosGetAdmin = async (token) => {
   const admin = res.data.data;
 
   return admin
+
+};
+
+export const axiosPatchAdmin = async (token, newName, newNickname, newPhone) => {
+
+  const res = await axios.patch(urladminId, {
+    headers: {
+      "Authorization": `Bearer ${token}`,
+      "name": newName,
+      "nickname": newNickname,
+      "phone": newPhone
+    }
+  })
+
+  const newAdim = res.data.data;
+
+  return newAdim
+
+};
+
+export const axiosDelAdmin = async (token) => {
+
+  const res = await axios.del(urladminId, {
+    headers: {
+      "Authorization": `Bearer ${token}`,
+    }
+  })
+
+  const delAdim = res.data.data;
+
+  return delAdim
 
 };

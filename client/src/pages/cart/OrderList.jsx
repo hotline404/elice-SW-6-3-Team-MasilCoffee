6708e3c3fc3e36a0344536abd762edac1233a782
김 다빈 <besesa95@gmail.com>
2023-11-28 00:27:11 +0900

    Add : Cart page OrderList update

--- client/src/pages/cart/OrderList.jsx
+++ client/src/pages/cart/OrderList.jsx
@@ -9,14 +9,35 @@ import { addOrder } from "../../redux/action/orderAction";
 const OrderList = ({ handleOnSelect, checkedStates }) => {
   const dispatch = useDispatch();
   const orders = useSelector((state) => state.order);
-  const pruducts = useSelector((state) => state.product.products);
+  const products = useSelector((state) => state.product.products);
   //   const [totalPrice, setTotalPrice] = useState();
+
+  const calculatePrice = (orderId, quantity) => {
+    const originPrice = products.find(({ id }) => orderId === id).price;
+    return originPrice * quantity;
+  };
 
   const handleQuantity = (order, operator) => {
     if (operator === "+") {
-      dispatch(updateOrder({ ...order, menu: order.menu + 1 }, order.id));
-    } else if (operator === "-" && order.menu > 1) {
-      dispatch(updateOrder({ ...order, menu: order.menu - 1 }, order.id));
+      dispatch(
+        updateOrder(
+          {
+            quantity: order.quantity + 1,
+            totalPrice: calculatePrice(order.orderId, order.quantity + 1),
+          },
+          order.orderId
+        )
+      );
+    } else if (operator === "-" && order.quantity > 1) {
+      dispatch(
+        updateOrder(
+          {
+            quantity: order.quantity - 1,
+            totalPrice: calculatePrice(order.orderId, order.quantity - 1),
+          },
+          order.orderId
+        )
+      );
     }
   };
 
@@ -34,17 +55,59 @@ const OrderList = ({ handleOnSelect, checkedStates }) => {
                 />
                 <b>{order.name}</b>
               </div>
-              <span>{`샷 : ${order.shot}`}</span>
-              {order.shot.map(item => (<span>`샷 : ${item.name} ${item.quantity}`</span>))}
+              {order.shot
+                .filter((item) => item.quantity > 0)
+                .map((item) => (
+                  <span
+                    key={item.name}
+                  >{`샷 : ${item.name} ${item.quantity}`}</span>
+                ))}
+              {order.syrups
+                .filter((item) => item.quantity > 0)
+                .map((item) => (
+                  <span
+                    key={item.name}
+                  >{`시럽 : ${item.name} ${item.quantity}`}</span>
+                ))}
+              {order.whipping
+                .filter((item) => item.quantity > 0)
+                .map((item) => (
+                  <span
+                    key={item.name}
+                  >{`휘핑 : ${item.name}`}</span>
+                ))}
+              {order.ice
+                .filter((item) => item.quantity > 0)
+                .map((item) => (
+                  <span
+                    key={item.name}
+                  >{`얼음 : ${item.name}`}</span>
+                ))}
+              {order.drizzle
+                .filter((item) => item.quantity > 0)
+                .map((item) => (
+                  <span
+                    key={item.name}
+                  >{`드리즐 : ${item.name}`}</span>
+                ))}
+              {order.milk
+                .filter((item) => item.quantity > 0)
+                .map((item) => (
+                  <span
+                    key={item.name}
+                  >{`우유 : ${item.name}`}</span>
+                ))}
+
+              {/* <span>{`샷 : ${order.shot}`}</span>
               <span>{`시럽 : 바닐라 ${order.syrups.vanilla} 헤이즐넛 ${order.syrups.hazelnut} 카라멜  ${order.syrups.caramel} `}</span>
               <span>{`얼음 : ${order.ice}`}</span>
               <span>{`휘핑 : ${order.whipping}`}</span>
               <span>{`드리즐 : ${order.drizzle}`}</span>
-              <span>{`우유 : ${order.milk}`}</span>
+              <span>{`우유 : ${order.milk}`}</span> */}
               <div>
                 <b>수량</b>
                 <button onClick={() => handleQuantity(order, "-")}>-</button>
-                <span>{order.menu}</span>
+                <span>{order.quantity}</span>
                 <button onClick={() => handleQuantity(order, "+")}>+</button>
                 <b>총 주문금액</b>
                 <b>{order.totalPrice}원</b>

dbb927ddff469014eaeb88abcbb7cb4699b5b850
김 다빈 <besesa95@gmail.com>
2023-11-28 21:23:30 +0900

    Add : payment api update

--- client/src/api/payment/payment.jsx
+++ client/src/api/payment/payment.jsx
@@ -17,7 +17,39 @@ export const postPayment = async (paymentData, token) => {
   }
 };
 
-// 경로 데이터
-// 헤더에 const headers = { headers: { Authorization: Bearer ${token} } };
-// 위의 것 을 보내야 함
-// 백쿼터
+// 전체주문 가져오기
+export const getAllPayment = async () => {
+  const res = await axios.get("http://localhost:5000/api/v1/order/admin");
+  const payments = res.data.data;
+  return payments;
+};
+
+// 사용자 주문 내역 가져오기
+export const getPayment = async (orderId) => {
+  const res = await axios.get(`http://localhost:5000/api/v1/order/${orderId}`);
+  const payment = res.data.data;
+  return payment;
+};
+
+// 주문정보 수정
+export const updatePayment = async (orderId) => {
+  const res = await axios.put(
+    `http://localhost:5000/api/v1/order/:orderId/${orderId}`
+  );
+  const payment = res.data.data;
+  return payment;
+};
+
+// 주문 삭제
+export const deletePayment = async (orderId) => {
+  try {
+    const res = await axios.delete(
+      `http://localhost:5000/api/v1/order/:orderId/${orderId}`
+    );
+    const payment = res.data.data;
+
+    return payment;
+  } catch (error) {
+    console.error(error);
+  }
+};

d5c0d75ea3b40510c961ec63964fd1b3dac8db37
김 다빈 <besesa95@gmail.com>
2023-11-29 16:50:25 +0900

    Add : Main page Bottom update

--- client/src/pages/main/components/Bottom.jsx
+++ client/src/pages/main/components/Bottom.jsx
@@ -1,3 +1,4 @@
+import { useNavigate, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -5,29 +6,40 @@ import * as S from "../components/style/Bottom.style";
 import SquareButton from "../../../components/ui/button/SquareButton";
 import { BsChat } from "react-icons/bs";
 
-// import { getAllBoards } from "../../../api/board";
+import { getAllBoards } from "../../../api/board";
 import { useDispatch, useSelector } from "react-redux";
 import { actionGetAllBoards } from "../../../redux/action/boardAction";
 
 const Bottom = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   // Redux 스토어에서 게시글 데이터 가져오기
   const allBoards = useSelector((state) => state.board);
-  console.log("올보더스", allBoards);
-  console.log("올보더스.보드", allBoards.board);
-  console.log("올보더스.보드.닉네임", allBoards.board.nickname);
 
   useEffect(() => {
     // dispatch(actionGetAllBoards(boards)); // 데이터 로드
-    console.log("올보더스", allBoards);
+    // console.log("올보더스", allBoards);
+    const fn = async () => {
+      try {
+        const board = await getAllBoards("", 1, 10);
+        dispatch(actionGetAllBoards(board));
+      } catch (err) {
+        console.log("err", err);
+      }
+    };
+    fn();
   }, [dispatch]);
+
+  const handleBoardClick = (boardId) => {
+    navigate(`/RecipeView/${boardId}`); // 게시글 상세 페이지로 이동
+  };
 
   // 슬라이더 설정
   const settings = {
     infinite: true,
     speed: 15000, // 슬라이드 전환 속도
     slidesToShow: 4, // 한 번에 보여줄 슬라이드 개수
-    autoplay: false, // 자동 슬라이드 활성화
+    autoplay: true, // 자동 슬라이드 활성화
     autoplaySpeed: 0, // 자동 슬라이드 간격
     cssEase: "linear", // 일정한 속도로 움직이도록 설정
   };
@@ -115,36 +127,49 @@ const Bottom = () => {
         {Array.isArray(allBoards.boards) &&
           allBoards.boards.map((board, index) => {
             // console.log("Board ID:", board._id);
-            const idNumber = parseInt(board._id);
-            const isOdd = idNumber % 2 === 0;
+            const isOdd = index % 2 === 0;
             const boxBgColor = isOdd ? "#d9d9d9" : fixedBackgroundColor; // 홀수이면 그레이, 아니면 기존 색상
 
             return (
-              <S.Bottom key={board._id}>
+              <S.Bottom
+                key={board._id}
+                onClick={() => handleBoardClick(board._id)}
+              >
                 <S.BottomBox $bgColor={boxBgColor} isEven={isOdd}>
                   <div>
-                    <b>{board._id}</b>
-                    <p>{board.boards}</p>
+                    <b>{board.nickname}</b>
+                    <p>{board.post}</p>
                   </div>
                   <S.SquareButtonBox>
                     <S.SquareButton>
                       <SquareButton
-                        text={"에스프레소"}
+                        text={board.category}
                         bgColor={backgroundColor}
                       />
                     </S.SquareButton>
-                    <S.SquareButton>
-                      <SquareButton
-                        text={"헤이즐넛"}
-                        bgColor={backgroundColor}
-                      />
-                    </S.SquareButton>
-                    <S.SquareButton>
-                      <SquareButton
-                        text={"아메리카노"}
-                        bgColor={backgroundColor}
-                      />
-                    </S.SquareButton>
+                    <S.SquareButtonBox>
+                      {board.tags.length > 0 ? (
+                        (() => {
+                          let iterations = Math.min(board.tags.length, 2); // 최대 2개의 태그만 처리
+
+                          let buttons = [];
+                          for (let i = 0; i < iterations; i++) {
+                            buttons.push(
+                              <S.SquareButton key={i}>
+                                <SquareButton
+                                  text={board.tags[i]}
+                                  bgColor={backgroundColor}
+                                />
+                              </S.SquareButton>
+                            );
+                          }
+
+                          return buttons;
+                        })()
+                      ) : (
+                        <></> // 태그가 없는 경우 아무것도 표시하지 않음
+                      )}
+                    </S.SquareButtonBox>
                   </S.SquareButtonBox>
                   <S.BsChat>
                     <BsChat />
